5556054140946308626da3974962509b1129ca1d
yan2nwpu <442019119@qq.com>
2021-07-31 00:27:33 +0800

    上传1题答案

--- 1_求int正整数在内存中1的个数/yan/main.cpp
+++ 1_求int正整数在内存中1的个数/yan/main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
-int main(){
+int main() {
+	int a = 0;
+	cin >> a;
+	if (a <= 0) {
+		return 1;
+	}
+	int count = 0;
+	while (a > 0) {
+		if (a % 2) {
+			count++;
+		}
+		a = a / 2;
+	}
+	cout << count << endl;
+	
 	return 0;
 }
